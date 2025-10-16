5940a49f3d3fb0cd90a96e39b3c6f3806345f139
masafumi terazono <terazono.masafumi@m-gild.com>
2025-10-16 20:41:54 +0900

    ダメージイベントがなにか調べるログをいれた

--- src/main/kotlin/com/hacklab/minecraft_legends/presentation/listener/DamageListener.kt
+++ src/main/kotlin/com/hacklab/minecraft_legends/presentation/listener/DamageListener.kt
@@ -1,5 +1,7 @@
 package com.hacklab.minecraft_legends.presentation.listener
 
+import com.hacklab.minecraft_legends.infrastructure.logger.Logger
+import com.hacklab.minecraft_legends.infrastructure.logger.LoggerFactory
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
@@ -7,13 +9,16 @@ import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageEvent
 
 class DamageListener : Listener {
-    
+    private val logger = LoggerFactory.getLogger()
+
     @EventHandler(priority = EventPriority.HIGHEST)
-    fun onPlayerDamage(event: EntityDamageEvent) {
+    fun onEntityDamage(event: EntityDamageEvent) {
         if (event.entity !is Player) return
         
         if (event.cause == EntityDamageEvent.DamageCause.FALL) {
             event.isCancelled = true
+        } else {
+            logger.debug("onEntityDamage ${event.cause}")
         }
     }
 }
